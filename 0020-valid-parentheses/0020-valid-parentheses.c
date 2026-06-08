1c7901d564a25a2070e16a66c9174ffdeb5fd8b7
Nguyễn Vũ Nhật Thành <thomasnguyen30072004@gmail.com>
2026-06-08 22:04:56 +0700

    Time: 0 ms (100%), Space: 8.8 MB (62.27%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.c
+++ 0020-valid-parentheses/0020-valid-parentheses.c
@@ -1,38 +1,28 @@
 #include <stdbool.h>
 #include <string.h>
 
-bool isValid(char* s) {
+bool isValid(char *s){
     int len = strlen(s);
-    
-    // Neu chieu dai chuoi le thi chac chan khong the khop cap
-    if (len % 2 != 0) return false;
-    
-    // Tao stack bang mang voi kich thuoc bang chieu dai chuoi
-    char stack[len];
-    int top = -1; // Stack rong khi top = -1
-    
-    for (int i = 0; i < len; i++) {
+    if (len % 2 != 0) return false; // Neu chieu dai khong chia het cho 2 thi le nen ko ghep cap duoc
+
+    char stack[len]; // Tao stack bang mang voi kich thuoc bang chieu dai chuoi
+    int top = -1; // Stack rong khi top = -1;
+
+    for (int i=0; i<len; i++){
         char c = s[i];
-        
-        // Neu la dau ngoat mo, push vao stack
-        if (c == '(' || c == '[' || c == '{') {
-            stack[++top] = c;
-        } 
-        // Neu la dau ngoat dong
-        else {
-            // Stack rong ma gap dau ngoat dong thi sai
+        if (c == '(' || c == '[' || c == '{'){
+            stack[++top] = c; // top++; stack[top] = c;
+        } else {
             if (top == -1) return false;
-            
-            // Lay phan tu o dinh stack ra de kiem tra
-            char openBracket = stack[top--];
-            
+
+            char open = stack[top--]; // Lay phan tu o dinh stack ra de kiem tra
+
             // Kiem tra xem co khop cap hay khong
-            if (c == ')' && openBracket != '(') return false;
-            if (c == ']' && openBracket != '[') return false;
-            if (c == '}' && openBracket != '{') return false;
+            if (c == ')' && open != '(') return false;
+            if (c == ']' && open != '[') return false;
+            if (c == '}' && open != '{') return false;
         }
     }
-    
-    // Neu top == -1 tuc la tat ca deu duoc khop cap hop le
-    return top == -1;
+
+    return top == -1; // Neu top = -1 => tat ca deu hop le
 }
