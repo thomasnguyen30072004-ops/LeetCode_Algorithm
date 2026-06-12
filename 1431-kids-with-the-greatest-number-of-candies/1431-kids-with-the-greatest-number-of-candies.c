041776cc4201b34888687c5f671b5f3365044119
Nguyễn Vũ Nhật Thành <thomasnguyen30072004@gmail.com>
2026-06-12 19:45:13 +0700

    Time: 0 ms (100%), Space: 10.4 MB (76.9%) - LeetHub

--- 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.c
@@ -5,23 +5,20 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 bool* kidsWithCandies(int* candies, int candiesSize, int extraCandies, int* returnSize) {
-    // Tim so keo lon nhat hien tai
     int maxCandies = candies[0];
-    for (int i = 1; i < candiesSize; i++) {
-        if (candies[i] > maxCandies) {
+    for (int i=0; i < candiesSize; i++){
+        if (candies[i] > maxCandies){
             maxCandies = candies[i];
         }
     }
 
     // Cap phat bo nho dong cho mang ket qua
-    bool* result = (bool*)malloc(candiesSize * sizeof(bool));
-    
-    // Gan kich thuoc mang tra ve cho Leetcode quan ly
+    bool *result = (bool *)malloc(candiesSize * sizeof(bool));
+
     *returnSize = candiesSize;
 
-    // Kiem tra tung dua tre sau khi nhan them keo extra
-    for (int i = 0; i < candiesSize; i++) {
-        if (candies[i] + extraCandies >= maxCandies) {
+    for (int i=0; i < candiesSize; i++){
+        if (candies[i] + extraCandies >= maxCandies){
             result[i] = true;
         } else {
             result[i] = false;
